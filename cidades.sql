1ff4ebe7abe3d749c53e96eef28b05a67404e0da
leonardobuck <leonardo@timais.com>
2019-12-12 13:00:12 -0300

    criando base de teste com a tabela de cidades

---
+++ cidades.sql
@@ -0,0 +1,10 @@
+create table cidades (
+	id INT,
+	cidade VARCHAR(50),
+	populacao INT
+);
+insert into cidades (id, cidade, populacao) values (1, 'Custódia', 683070);
+insert into cidades (id, cidade, populacao) values (2, 'Lukavec', 739089);
+insert into cidades (id, cidade, populacao) values (3, 'Campaka', 879598);
+insert into cidades (id, cidade, populacao) values (4, 'Cibunar', 474121);
+insert into cidades (id, cidade, populacao) values (5, 'Östersund', 602027);
