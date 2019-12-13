30abff357e7271b5c7bb04b0b54c3db1459fa241
leonardobuck <leonardo@timais.com>
2019-12-13 13:07:41 -0300

    Gráfico de Colunas

---
+++ segundoGrafico.php
@@ -0,0 +1,55 @@
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        // dados
+        var data = google.visualization.arrayToDataTable([
+          ['Cidade', 'População', {role: 'style'}],
+          <?php
+
+            include 'conexao.php';
+            $sql = "SELECT * FROM cidades";
+            $buscar = mysqli_query($conexao, $sql);
+
+            while($dados = mysqli_fetch_array($buscar)) {
+              $cidade = $dados['cidade'];
+              $populacao = $dados['populacao'];
+            
+          ?>
+
+          ['<?php echo $cidade; ?>',  <?php echo $populacao; ?>, '#000'],
+
+          <?php
+            }
+          ?>
+        ]);
+
+        var view = new google.visualization.DataView(data);
+        view.setColumns([0, 1,
+                        { calc: "stringify",
+                          sourceColumn: 1,
+                          type: "string",
+                          role: "annotation" },
+                        2]);
+
+        var options = {
+          title: "População das Cidades",
+          width: 700,
+          height: 400,
+          bar: {groupWidth: "75%"},
+          legend: { position: "right" },
+        };
+        var chart = new google.visualization.ColumnChart(document.getElementById("graficoColuna"));
+        chart.draw(view, options);
+    }
+    </script>
+  </head>
+  <body>
+    <div id="graficoColuna" style="width: 900px; height: 500px"></div>
+  </body>
+</html>
