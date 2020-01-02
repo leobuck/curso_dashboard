338b63ba122ff5920f4d82ffd43b918d9daa13c7
leobuck <leobuck12@gmail.com>
2020-01-02 16:40:14 -0300

    Dashboard Responsivo

--- dashboard.php
+++ dashboard.php
@@ -10,8 +10,18 @@
     <title>Curso Dashboard</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <style>
+      @media only screen and (max-width: 600px) {
+        body {
+          width: 300px;
+          margin-bottom: 70px;
+          text-align: center;
+        }
+      }
+    </style>
   <body>
-    <nav class="navbar navbar-dark bg-dark flex-md-nowrap p-0 shadow">
+    <nav class="navbar navbar-dark bg-dark flex-md-nowrap p-0 shadow d-none d-sm-block">
       <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="">Curso Dashboard</a>
       <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
@@ -86,8 +96,48 @@
       </div>
     </div>
 
+    <div class="d-block d-sm-none">
+      <nav class="navbar fixed-bottom navbar-light bg-light">
+        <a class="navbar-brand" href="#">Dashboard</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" 
+          aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link active" href="?pagina">
+                Dashboard <span class="sr-only">(atual)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="?pagina=pedidos" style="text-decoration: none;">
+                Pedidos
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="?pagina=clientes" style="text-decoration: none;">
+                Cadastro de Clientes
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="?pagina=vendas" style="text-decoration: none;">
+                Cadastro de Vendas
+              </a>
+            </li>
+          </ul>        
+        </div>
+      </nav>
+    </div>
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
+    <script>
+      $(window).resize(function(){
+        drawChart();
+        drawChart2();
+      })
+    </script>
 </body></html>
--- painel2.php
+++ painel2.php
@@ -38,9 +38,9 @@
     </script>
     <script type="text/javascript">
       google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
+      google.charts.setOnLoadCallback(drawChart2);
 
-      function drawChart() {
+      function drawChart2() {
 
         var data = google.visualization.arrayToDataTable([
           ['Task', 'Hours per Day'],
